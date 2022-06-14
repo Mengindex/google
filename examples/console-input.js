721db5ae95c1fa30c616ca97276080e8fe963d7f
Mengindex <107460037+Mengindex@users.noreply.github.com>
2022-06-14 15:37:33 +0700

    Update console-input.js

--- examples/console-input.js
+++ examples/console-input.js
@@ -6,7 +6,7 @@ const GoogleAssistant = require('../index');
 
 const config = {
   auth: {
-    keyFilePath: path.resolve(__dirname, 'YOUR_KEY_FILE.json'),
+    keyFilePath: path.resolve(__dirname, 'credentials.json'),
     savedTokensPath: path.resolve(__dirname, 'tokens.json'), // where you want the tokens to be saved
   },
   conversation: {
